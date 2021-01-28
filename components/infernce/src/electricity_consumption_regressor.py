a4911502ab5d82c336a4d6d043fba7d66ea0b984
Enrico Salvucci <e.salvucci@reply.it>
2021-01-28 16:34:18 +0100

    Change the model and fix empty result from bentoml endpoint

--- components/infernce/src/electricity_consumption_regressor.py
+++ components/infernce/src/electricity_consumption_regressor.py
@@ -1,16 +1,14 @@
 import pandas as pd
-
 from bentoml import env, artifacts, api, BentoService
 from bentoml.adapters import DataframeInput
-from bentoml.frameworks.xgboost import XgboostModelArtifact
-from xgboost import DMatrix
+from bentoml.frameworks.sklearn import SklearnModelArtifact
 from singleton_logger import SingletonLogger
 
 logger = SingletonLogger.get_logger()
 
 
 @env(infer_pip_packages=True)
-@artifacts([XgboostModelArtifact('model')])
+@artifacts([SklearnModelArtifact('model')])
 class ElectricityConsumptionRegressorService(BentoService):
     """
     A minimum prediction service exposing a XGBoost model
@@ -23,15 +21,5 @@ class ElectricityConsumptionRegressorService(BentoService):
         how HTTP requests or CSV files are converted to a pandas Dataframe object as the
         inference API function input
         """
-        features = ['month_1', 'month_2', 'month_3', 'month_4', 'month_5', 'month_6', 'month_7', 'month_8', 'month_9',
-                    'month_10', 'month_11', 'month_12', 'weekday_0', 'weekday_1', 'weekday_2', 'weekday_3', 'weekday_4',
-                    'weekday_5', 'weekday_6', 'hour_0', 'hour_1', 'hour_2', 'hour_3', 'hour_4', 'hour_5', 'hour_6',
-                    'hour_7', 'hour_8', 'hour_9', 'hour_10', 'hour_11', 'hour_12', 'hour_13', 'hour_14', 'hour_15',
-                    'hour_16', 'hour_17', 'hour_18', 'hour_19', 'hour_20', 'hour_21', 'hour_22', 'hour_23', 'holiday',
-                    'load_lag_24', 'load_lag_25', 'load_lag_26', 'load_lag_27', 'load_lag_28', 'load_lag_29',
-                    'load_lag_30', 'load_lag_31', 'load_lag_32', 'load_lag_33', 'load_lag_34', 'load_lag_35',
-                    'load_lag_36', 'load_lag_37', 'load_lag_38', 'load_lag_39', 'load_lag_40', 'load_lag_41',
-                    'load_lag_42', 'load_lag_43', 'load_lag_44', 'load_lag_45', 'load_lag_46', 'load_lag_47',
-                    'load_lag_48']
-        data = pd.DataFrame(df, columns=features)
-        return self.artifacts.model.predict(DMatrix(data))
+        data = pd.DataFrame(df)
+        return self.artifacts.model.predict(data)
--- components/xgboost_model_training/src/model_training.py
+++ components/xgboost_model_training/src/model_training.py
@@ -1,14 +1,12 @@
 from mlflow.entities.model_registry.model_version_stages import STAGE_PRODUCTION
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import ParameterGrid
-from xgboost import XGBRegressor, DMatrix
-from xgboost import cv as xgb_cv
 import pandas as pd
 import fire
 import numpy as np
 from singleton_logger import SingletonLogger
 import mlflow
-import mlflow.xgboost
 from mlflow.tracking import MlflowClient
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import (
@@ -17,7 +15,7 @@ from sklearn.preprocessing import (
 
 
 logger = SingletonLogger.get_logger()
-model_name = "xgb_regressor"
+model_name = "random_forest_regressor"
 params_grid = dict(
     eta=[0.05, 0.1, 0.3],
     max_depth=[2, 4, 6],
@@ -48,29 +46,8 @@ def __fit_prep_pipeline(df):
     return feature_names, prep_pipeline
 
 
-def __xgb_grid_search_cv(params_grid, X, y, nfold, num_boost_round=1000, early_stopping_rounds=10):
-    params_grid = ParameterGrid(params_grid)
-    search_results = []
-    logger.info(f"Grid search CV : nfold={nfold}, " +
-          f"numb_boost_round={num_boost_round}, " +
-          f"early_stopping_round={early_stopping_rounds}")
-    for params in params_grid:
-        logger.info(f"\t{params}")
-        cv_df = xgb_cv(
-            params=params, dtrain=DMatrix(X, y), nfold=nfold,
-            num_boost_round=num_boost_round,
-            early_stopping_rounds=early_stopping_rounds,
-            shuffle=False, metrics="rmse",
-        )
-        cv_results = params.copy()
-        cv_results["train-rmse-mean"] = cv_df["train-rmse-mean"].min()
-        cv_results["test-rmse-mean"] = cv_df["test-rmse-mean"].min()
-        search_results.append(cv_results)
-    return pd.DataFrame(search_results)
-
-
 def __save(model, name):
-    mlflow.xgboost.log_model(model, artifact_path="XGB_regressor_model", registered_model_name=name)
+    mlflow.sklearn.log_model(model, artifact_path="random_forest_regressor", registered_model_name=name)
     logger.info("Model saved")
 
 
@@ -103,15 +80,13 @@ def __promote(name):
     client.transition_model_version_stage(name, version=2, stage=STAGE_PRODUCTION)
 
 
-def train_model(training_set_path, test_set_path, original_dataset_path,
-                n_estimators, learning_rate, max_depth, min_child_weight, version, use_cv):
+def train_model(training_set_path, test_set_path, original_dataset_path):
     training_dataset = pd.read_csv(training_set_path)
     test_dataset = pd.read_csv(test_set_path)
     x_training_set, y_training_set = __split_data_into_x_y(training_dataset)
     x_test_set, y_test_set = __split_data_into_x_y(test_dataset)
 
     feature_names, prepared_data = __fit_prep_pipeline(x_training_set)
-    logger.info(feature_names)
     x_training_set = prepared_data.transform(x_training_set)
     x_training_set = pd.DataFrame(x_training_set, columns=feature_names, index=training_dataset.index)
 
@@ -120,40 +95,15 @@ def train_model(training_set_path, test_set_path, original_dataset_path,
 
     experiment = __get_mlflow_experiment(model_name)
 
-    with mlflow.start_run(experiment_id=experiment.experiment_id, run_name=version):
-        if use_cv:
-            xgb_search_scores = __xgb_grid_search_cv(
-                params_grid, x_training_set, y_training_set, nfold=4, early_stopping_rounds=10
-            )
-
-            parameters_dic = xgb_search_scores.sort_values(by="test-rmse-mean").head(1)
-            learning_rate = float(parameters_dic.eta)
-            max_depth = int(parameters_dic.max_depth)
-            min_child_weight = int(parameters_dic.min_child_weight)
-
-        __log_parameter("N estimators", n_estimators)
-        __log_parameter("Learning Rate", learning_rate)
-        __log_parameter("Max Depth", max_depth)
-        __log_parameter("Min child weight", min_child_weight)
-
-        model = XGBRegressor(
-            n_estimators=n_estimators,
-            learning_rate=learning_rate,
-            max_depth=max_depth,
-            min_child_weight=min_child_weight
+    with mlflow.start_run(experiment_id=experiment.experiment_id):
+        model = RandomForestRegressor(
+            n_estimators=100, criterion='mse', min_samples_leaf=0.001, random_state=42
         )
 
         logger.info("Training Started")
-        model.fit(
-            x_training_set, y_training_set, early_stopping_rounds=10,
-            eval_set=[(x_training_set, y_training_set), (x_test_set, y_test_set)],
-            verbose=True,
-        )
+        model.fit(x_training_set, y_training_set)
 
         y_test_pred = model.predict(x_test_set)
-        logger.info("x_test_set")
-        pd.set_option("display.max_rows", None, "display.max_columns", None)
-        logger.info(x_test_set.head(1).to_csv())
         rmse = np.sqrt(mean_squared_error(y_test_pred, y_test_set))
         __log_metric("rmse", rmse)
 
